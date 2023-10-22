968ac61cfcf088f1a78644a3f50ead80f9de8f04
Faggenza <ik3cco@gmail.com>
2023-10-22 23:47:33 +0200

    adding harpoon and some remap

--- lua/faggi/remap.lua
+++ lua/faggi/remap.lua
@@ -1,5 +1,18 @@
 vim.g.mapleader= " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+--Move block of code
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+--Cursor always at center
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- global and local yank
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+
